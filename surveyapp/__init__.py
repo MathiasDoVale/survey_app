134ea6f52c21cf6181b7e629b9f2e08d7fd0641f
Mathias <matdovale@gmail.com>
2020-11-19 21:07:08 -0300

    Encripto password y creo condicion si no existe la db

--- surveyapp/__init__.py
+++ surveyapp/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # SETTINGS
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 from surveyapp import routes
 
 
--- surveyapp/routes.py
+++ surveyapp/routes.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, session, logging, url_for, redirect, flash
 from surveyapp.models import User
-from surveyapp import app, db
-from flask_login import LoginManager, login_required, login_user, logout_user, login_manager, current_user
+from surveyapp import app, db, login_manager, bcrypt
+from flask_login import login_required, login_user, logout_user, current_user
 from surveyapp.forms import RegistrationForm, LoginForm
+import os
 
-db.create_all()
-
-login_manager = LoginManager()
-login_manager.init_app(app)
+# Chequeo si existe la bd
+if not os.path.isfile('surveyapp/test.db'):
+    db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -26,12 +26,15 @@ def home():
 
 @app.route("/register", methods=["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.email.data}!', 'success')
         email = request.form.get("email")
         password = request.form.get("password")
-        new_user = User(email=email, password=password)
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -43,8 +46,8 @@ def login():
     if form.validate_on_submit():
         email = request.form.get("email")
         password = request.form.get("password")
-        user = User.query.filter_by(email=email, password=password).first()
-        if user is not None:
+        user = User.query.filter_by(email=email).first()
+        if user is not None and bcrypt.check_password_hash(user.password, password):
             login_user(user, remember=True)
             db.session.add(user)
             db.session.commit()
